274e8ea2c80d567004c1db39995dd781b9560e57
Pavel R <334554.rud@gmail.com>
2025-08-13 17:50:11 +0300

    keyboards file has restructurized

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,9 +1,12 @@
 from aiogram.utils.keyboard import ReplyKeyboardMarkup
 from lexicon.lexicon import LEXICON_RU
 
+# list of buttons
+main_buttons = [[LEXICON_RU["stone"]], [LEXICON_RU["scissors"]], [LEXICON_RU["paper"]]]
+
 # main keyboard
 main_keyboard = ReplyKeyboardMarkup(
-    keyboard=[[LEXICON_RU["stone"]], [LEXICON_RU["scissors"]], [LEXICON_RU["paper"]]],
+    keyboard=main_buttons,
     resize_keyboard=True,
     input_field_placeholder="Просто нажмите кнопку",
 )
